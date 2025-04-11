42fe0fe56b1a0c81b74ad06b4bcf6df7fc9e6e21
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-04-11 15:07:18 +0900

    20250411

---
+++ 07LISTENER/src/main/java/Listener/C01ServletContextListener.java
@@ -0,0 +1,30 @@
+package Listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class C01ServletContextListener implements ServletContextListener{
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) { //처음 구동될 때 사용되어야 하는 것들이 있다면
+		//444
+		System.out.println("[LISTENER]C01ServletContextListener..start");
+	}
+
+	
+	
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		System.out.println("[LISTENER]C01ServletContextListener..end");
+		
+	}
+	
+	
+	
+	
+
+
+		
+}
---
+++ 07LISTENER/src/main/java/Listener/C02ServletContextAttributeListener.java
@@ -0,0 +1,23 @@
+package Listener;
+
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+
+public class C02ServletContextAttributeListener implements ServletContextAttributeListener {
+
+	@Override
+	public void attributeAdded(ServletContextAttributeEvent event) {
+	
+	}
+
+	@Override
+	public void attributeRemoved(ServletContextAttributeEvent event) {
+		
+	}
+
+	@Override
+	public void attributeReplaced(ServletContextAttributeEvent event) {
+		
+	}
+		
+}

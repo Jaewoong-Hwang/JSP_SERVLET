08819a6d91efb30cc585cc074ae9e6310eaa6a4b
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-04-14 09:56:46 +0900

    20250414

---
+++ 09MVC_INIT/src/main/java/Controller/FrontController.java
@@ -0,0 +1,46 @@
+package Controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Controller.user.UserCreateController;
+
+@WebServlet("/")
+public class FrontController extends HttpServlet {
+	//서브컨트롤러 저장 자료구조("/endPoint":서브컨트롤러객체)
+		private Map<String,SubController> map = new HashMap();
+
+		@Override
+		public void init(ServletConfig config) throws ServletException {
+		
+		//인증(/user/*) - 회원CRUD, 로그인, 로그아웃 
+			map.put("/user/create",new UserCreateController());
+			
+		//도서(/book/*) - 도서CRUD
+		
+		}
+
+		
+		
+		@Override
+		protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	
+		System.out.println("[FC] service...");
+		String endPoint = req.getServletPath(); // ProjectPath 제외 EndPoint만
+		System.out.println("[FC] endPoint.." + endPoint);
+		SubController controller =  map.get(endPoint);	//요청사항을 처리할 SubController get
+		controller.execute(req,resp);		 
+		
+		}
+
+		
+		
+}
---
+++ 09MVC_INIT/src/main/java/Controller/user/UserCreateController.java
@@ -0,0 +1,21 @@
+package Controller.user;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Controller.SubController;
+
+public class UserCreateController implements SubController{
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse resp) {
+		System.out.println("[SC] UserCreateController execute...");
+		
+		//파라미터 
+		//입력값
+		//서비스
+		//뷰
+		
+	}
+
+}

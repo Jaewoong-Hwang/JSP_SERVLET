d28c398945dc9b5166db6f24b2bb808a376708d9
Jaewoong-Hwang <boogiewoong2@gmail.com>
2025-04-14 10:39:06 +0900

    20250414

--- 09MVC_INIT/src/main/java/Controller/user/UserCreateController.java
+++ 09MVC_INIT/src/main/java/Controller/user/UserCreateController.java
@@ -4,18 +4,63 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import Controller.SubController;
+import Domain.Dto.UserDto;
 
-public class UserCreateController implements SubController{
+public class UserCreateController implements SubController {
+
+	HttpServletRequest req;
+	HttpServletResponse resp;
 
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse resp) {
+		this.req = req;
+		this.resp = resp;
+
 		System.out.println("[SC] UserCreateController execute...");
-		
-		//파라미터 
-		//입력값
-		//서비스
-		//뷰
-		
+
+		try {
+			String uri = req.getMethod();
+			if (uri.equals("GET")) {
+
+				req.getRequestDispatcher("/WEB-INF/view/user/create.jsp").forward(req, resp);
+				return;
+			}
+			// 파라미터(username,password)
+			String username = req.getParameter("username");
+			String password = req.getParameter("password");
+			String role = "ROLE_USER";
+			// 입력값검증
+			UserDto userDto = new UserDto(username, password, role);
+			boolean isOk = isValid(userDto);
+			if (!isOk) {
+				req.getRequestDispatcher("/WEB-INF/view/user/create.jsp").forward(req, resp);
+				return;
+			}
+			// 서비스
+			// 뷰
+		} catch (Exception e) {
+
+		}
+	}
+
+	private boolean isValid(UserDto userDto) {
+
+		if (userDto.getUsername() == null || userDto.getUsername().length() <= 4) {
+			req.setAttribute("username_error", "userid의 길이는 최소 5자이상이어야합니다");
+			System.out.println("[INVALID] userid의 길이는 최소 5자이상이어야합니다");
+			return false;
+		}
+		if (userDto.getUsername().matches("^[0-9].*")) {
+			System.out.println("[INVALID] userid의 첫문자로 숫자가 들어올수 없습니다");
+			req.setAttribute("username_error", "userid의 userid의 첫문자로 숫자가 들어올수 없습니다");
+			return false;
+		}
+		// NULL 체크 / 데이터(자료)수준에서의 의미있는데이터가 포함되어져있는지 여부
+		// userid 은 첫문자가 숫자인지 여부 - /or 길이가 1글자인지 등등..
+		// username 은 첫문자가 숫자인지 여부 -
+		// password 복잡도체크는 Business Layer 체크(Policy 에 의한 처리)
+
+		return true;
 	}
 
 }
